b392773f4973d53c39d477dc9f983d75f85e7d87
kevin1839 <36816472+kevin1839@users.noreply.github.com>
2021-03-14 19:54:58 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php 
 	session_start();
 	
-    $conn=mysqli_connect('localhost','root','','aplicacion1');
+    $conn=mysqli_connect('localhost','b1c0d093c437a3','ed56b687','heroku_e72c0d3538bd70c');
 ?>
