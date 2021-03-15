348b236046b12fd1675c09db8a9495e1c9e22dfc
kevin1839 <36816472+kevin1839@users.noreply.github.com>
2021-03-14 20:14:30 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php 
 	session_start();
 	
-    $conn=mysqli_connect('localhost','b1c0d093c437a3','ed56b687','heroku_e72c0d3538bd70c');
+    $conn=mysqli_connect('us-cdbr-east-03.cleardb.com','b1c0d093c437a3','ed56b687','heroku_e72c0d3538bd70c');
 ?>
